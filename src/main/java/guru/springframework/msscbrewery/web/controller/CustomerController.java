fe3787b2d58dfa773cb5d25d34fdda450e3e4db9
sebastianlopez <sebalopez2311@gmail.com>
2020-07-27 17:03:59 -0300

    Added requested Controller, service, and model objects for Customer. Added Controller Test

---
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,26 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import guru.springframework.msscbrewery.services.CustomerService;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> getById(@PathVariable UUID customerId) {
+        return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+}
